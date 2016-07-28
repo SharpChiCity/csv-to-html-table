760dd4e08118add3c9db53824403c358782a197a
kevin <sharpchicity@gmail.com>
2016-07-28 17:34:50 -0500

    2016-07-2

---
+++ test_git.py
@@ -0,0 +1,15 @@
+import os
+from datetime import datetime
+
+
+os.system("c://users//monstar/python//stubhub//cubs//stubhub_make_min_pricing_file.py")
+
+commit_message = str(datetime.today())[0:10]
+
+os.system("git add .")
+os.system('git commit -m "{}"'.format(commit_message))
+os.system("git push origin master")
+
+os.system('git checkout gh-pages')
+os.system('git merge -m "{}"'.format(commit_message))
+os.system("git push origin gh-pages")
