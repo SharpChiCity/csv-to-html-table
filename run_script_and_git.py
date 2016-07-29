2921665018c361ed27ab9b2b934b2e3506ab3c9b
SharpChiCity <sharpchicity@gmail.com>
2016-07-29 07:26:23 -0500

    Update run_script_and_git.py

--- run_script_and_git.py
+++ run_script_and_git.py
@@ -1,10 +1,9 @@
 import os
 from datetime import datetime
 
+execfile("c://users//monstar/python//stubhub//cubs//stubhub_make_min_pricing_file.py")
 
-# execfile("c://users//monstar/python//stubhub//cubs//stubhub_make_min_pricing_file.py")
-
-commit_message = str(datetime.today())[0:9]
+commit_message = str(datetime.today())[0:10]
 
 os.system("git add .")
 os.system('git commit -m "{}"'.format(commit_message))
