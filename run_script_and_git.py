9264de2db7001990a7595a2c9ca27fe9d5e6ac75
kevin <sharpchicity@gmail.com>
2016-08-24 08:05:08 -0500

    to run remote

--- run_script_and_git.py
+++ run_script_and_git.py
@@ -2,14 +2,14 @@ import os
 from datetime import datetime
 
 
-os.system("c://users//monstar/python//stubhub//cubs//stubhub_make_min_pricing_file.py")
+# os.system("c://users//monstar/python//stubhub//cubs//stubhub_make_min_pricing_file.py")
 
 commit_message = str(datetime.today())[0:10]
 
-os.system("git add .")
-os.system('git commit -m "{}"'.format(commit_message))
-os.system("git push origin master")
+os.system('git --git-dir=C://Users//Monstar//Python//Stubhub//cubs-bleacher-prices//.git add "C://Users//Monstar//Python//Stubhub//cubs-bleacher-prices//data//github prices.csv"')
+os.system('git --git-dir=C://Users//Monstar//Python//Stubhub//cubs-bleacher-prices//.git commit -m "{}"'.format(commit_message))
+os.system("git --git-dir=C://Users//Monstar//Python//Stubhub//cubs-bleacher-prices//.git push origin master")
 
-os.system('git checkout gh-pages')
-os.system('git merge -m "{}"'.format(commit_message))
-os.system("git push origin gh-pages")
+os.system('git --git-dir=C://Users//Monstar//Python//Stubhub//cubs-bleacher-prices//.git checkout gh-pages')
+os.system('git --git-dir=C://Users//Monstar//Python//Stubhub//cubs-bleacher-prices//.git merge -m "{}"'.format(commit_message))
+os.system("git --git-dir=C://Users//Monstar//Python//Stubhub//cubs-bleacher-prices//.git push origin gh-pages")
