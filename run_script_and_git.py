4ee0746f5e5c7b200e81a18c516ed93b67b59e48
root <root@ip-172-31-0-193.us-east-2.compute.internal>
2018-03-11 18:22:06 +0000

    set to run on linux

--- run_script_and_git.py
+++ run_script_and_git.py
@@ -6,20 +6,20 @@ while True:
 
     print(datetime.today())
     os.system('echo -----------{}'.format(datetime.today()))
-    os.system("c://users//monstar/python//stubhub//bot//season_tickets//upload_bleacher_prices.py")
+    os.system("sudo python3 /home/ec2-user/sh_scraper/season_tickets/upload_bleacher_prices.py")
 
     commit_message = str(datetime.today())[0:10]
 
-    os.system('git --git-dir=C://Users//Monstar//Python//Stubhub//cubs-bleacher-prices//.git checkout master')
-    os.system('git --git-dir=C://Users//Monstar//Python//Stubhub//cubs-bleacher-prices//.git add "C://Users//Monstar//Python//Stubhub//cubs-bleacher-prices//data//github_prices.csv"')
-    os.system('git --git-dir=C://Users//Monstar//Python//Stubhub//cubs-bleacher-prices//.git commit -m "{}"'.format(commit_message))
-    os.system("git --git-dir=C://Users//Monstar//Python//Stubhub//cubs-bleacher-prices//.git push origin master")
+    os.system('sudo git --git-dir=/home/ec2-user/cubs-bleacher-prices/.git checkout master')
+    os.system('sudo git --git-dir=/home/ec2-user/cubs-bleacher-prices/.git add "/home/ec2-user/cubs-bleacher-prices/data/github_prices.csv"')
+    os.system('sudo git --git-dir=/home/ec2-user/cubs-bleacher-prices/.git commit -m "{}"'.format(commit_message))
+    os.system("sudo git --git-dir=/home/ec2-user/cubs-bleacher-prices/.git push origin master")
 
-    os.system('git --git-dir=C://Users//Monstar//Python//Stubhub//cubs-bleacher-prices//.git checkout gh-pages')
-    os.system('git --git-dir=C://Users//Monstar//Python//Stubhub//cubs-bleacher-prices//.git merge origin master -m "{}"'.format(commit_message))
-    os.system("git --git-dir=C://Users//Monstar//Python//Stubhub//cubs-bleacher-prices//.git push origin gh-pages")
+    os.system('sudo git --git-dir=/home/ec2-user/cubs-bleacher-prices/.git checkout gh-pages')
+    os.system('sudo git --git-dir=/home/ec2-user/cubs-bleacher-prices/.git merge origin master -m "{}"'.format(commit_message))
+    os.system("sudo git --git-dir=/home/ec2-user/cubs-bleacher-prices/.git push origin gh-pages")
 
-    os.system('git --git-dir=C://Users//Monstar//Python//Stubhub//cubs-bleacher-prices//.git checkout master')
+    os.system('sudo git --git-dir=/home/ec2-user/cubs-bleacher-prices/.git checkout master')
     
     print('done')
     os.system('echo ----------- sleeping')
