f9d2acc349706a307d490bf866eeae258bbf1a07
root <root@ip-172-31-9-168.us-east-2.compute.internal>
2019-03-30 14:49:36 +0000

    rebase

---
+++ run_script_and_git.py
@@ -0,0 +1,28 @@
+import os
+from datetime import datetime
+import time
+
+while True:
+
+    print(datetime.today())
+    os.system('echo -----------{}'.format(datetime.today()))
+    os.system("sudo python3 /home/ec2-user/sh_scraper/season_tickets/upload_bleacher_prices.py")
+
+    commit_message = str(datetime.today())[0:10]
+
+    os.system('sudo git --git-dir=/home/ec2-user/cubs-bleacher-prices/.git checkout master')
+    os.system('sudo git --git-dir=/home/ec2-user/cubs-bleacher-prices/.git add "/home/ec2-user/cubs-bleacher-prices/data/github_prices.csv"')
+    os.system('sudo git --git-dir=/home/ec2-user/cubs-bleacher-prices/.git commit -m "{}"'.format(commit_message))
+    os.system("sudo git --git-dir=/home/ec2-user/cubs-bleacher-prices/.git push origin master")
+
+    os.system('sudo git --git-dir=/home/ec2-user/cubs-bleacher-prices/.git checkout gh-pages')
+    os.system('sudo git --git-dir=/home/ec2-user/cubs-bleacher-prices/.git merge origin master -m "{}"'.format(commit_message))
+    os.system("sudo git --git-dir=/home/ec2-user/cubs-bleacher-prices/.git push origin gh-pages")
+
+    os.system('sudo git --git-dir=/home/ec2-user/cubs-bleacher-prices/.git checkout master')
+    
+    print('done')
+    os.system('echo ----------- sleeping')
+    print(datetime.today())
+    time.sleep(3600)
+
